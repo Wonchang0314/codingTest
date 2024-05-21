104ae53ce6bde01177094c71061e72badf742f06
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-05-22 06:16:46 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,15 @@
+function solution(citations) {
+    var answer = 0;
+    citations.sort((a, b) => b - a);
+    let hIndex = 0;
+    for(let i = 0; i < citations.length; i++){
+        if(i+1 <= citations[i]){
+            hIndex = i + 1
+        }
+        else{
+            break;
+        }
+    }
+    
+    return hIndex
+}
