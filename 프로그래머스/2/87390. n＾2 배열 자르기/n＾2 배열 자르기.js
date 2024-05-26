c816652075f667edc92dfdba2efc86bc228de96b
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-05-27 01:46:22 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.16 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,9 @@
+function solution(n, left, right) {
+    var answer = [];
+    for(let i = left; i <= right; i++){
+        let j = Math.floor(i / n);
+        let k = i % n;
+        answer.push(Math.max(j, k) + 1);
+    }
+    return answer;
+}
