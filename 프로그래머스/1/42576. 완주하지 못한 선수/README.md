01b96dc93bef7428a29826789150d786a15c80a0
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-05-29 00:23:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 32.73 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 57 MB, 시간: 90.43 ms
+메모리: 67.3 MB, 시간: 32.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 24일 03:07:15
+2024년 05월 29일 00:23:55
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,25 @@
 function solution(participant, completion) {
     var answer = '';
-    participant.sort();
-    completion.sort();
-    
-    for (var i = 0; i < participant.length; i++) {
-        if(i == completion.length){
-            answer = participant[i];
-            break;
+    let map = new Map();
+
+    for(let i = 0; i < participant.length; i++){
+        if(!map.has(participant[i])){
+            map.set(participant[i], 1);
         }
-        else if (participant[i] !== completion[i]) {
-            return participant[i];
+        else{
+            map.set(participant[i], map.get(participant[i]) + 1);
         }
     }
     
-    return answer;
+    for(let i = 0; i < completion.length; i++){
+        if(map.has(completion[i])){
+            if(map.get(completion[i]) > 1){
+                map.set(completion[i], map.get(completion[i]) - 1);
+            }
+            else{
+                map.delete(completion[i]);
+            }
+        }
+    }
+    return map.keys().next().value;
 }
