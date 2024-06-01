9976c75c587a2c55a6e9f6ae84f33bacc80fe3bf
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-06-01 21:25:10 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 915.56 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,11 +2,12 @@ function solution(elements) {
     let answer = new Set();
     let n = elements.length;
     
+    let extended = elements.concat(elements);
     for (let length = 1; length <= n; length++) {
         for(let i = 0; i < n; i++){
             let sum = 0;
             for(let j = 0; j < length; j++){
-                sum += elements[(i + j) % n];
+                sum += extended[i + j];
             }
             answer.add(sum);
         }
