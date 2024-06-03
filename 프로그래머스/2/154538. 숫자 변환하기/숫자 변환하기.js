02075fa6969fec888d003c3f3853ce2a041422e9
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-06-04 00:51:54 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.11 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,26 @@
+function solution(x, y, n) {
+    let answer = [];
+    let index = 0
+    let queue = [[x, 0]];
+    let tried = new Set();
+    while(queue.length && index < queue.length){
+        let [num, count] = queue[index];
+        index += 1;
+        if(num === y){
+            return count;
+        }
+        if(num + n <= y && !tried.has(num + n)){
+            queue.push([num + n, count + 1]);
+            tried.add(num + n);
+        }
+        if(num * 2 <= y && !tried.has(num * 2)){
+            queue.push([num * 2, count + 1]);
+            tried.add(num * 2);
+        }
+        if(num * 3 <= y && !tried.has(num * 3)){
+            queue.push([num * 3, count + 1]);
+            tried.add(num * 3);
+        }
+    }
+    return -1;
+}
