314dc59de910bd1571edd7bb8782dbb5829c8415
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-06-25 18:17:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,20 +1,31 @@
 function solution(n, words) {
     let answer = [0, 0];
-    let wordSet = new Set();
-    let prev = words[0];
-    wordSet.add(prev);
-    
-    
-    for(let i = 1; i < words.length; i++){
-        let current = words[i];
-        if(wordSet.has(current) || prev.charAt(prev.length - 1) !== current.charAt(0)){
-            let turn = Math.floor(i / n) + 1;
-            let player = (i % n) + 1;
-            return [player, turn];
+    let used = new Map();
+    words.forEach(word => used.set(word, false));
+    let turn = 1;
+    let round = 1;
+    let prev = '';
+    for(let i = 0; i < words.length; i++){
+        let word = words[i];
+        if(used.get(word) === true){
+            answer[0] = turn;
+            answer[1] = round;
+            return answer;
         }
-        wordSet.add(current);
-        prev = current;
+        else if(prev !== '' && !word.startsWith(prev[prev.length-1])){
+            answer[0] = turn;
+            answer[1] = round;
+            return answer;
+        }
+        else {
+            used.set(word, true);
+            prev = word;
+            if(turn === n){
+                round += 1;
+                turn = 1;
+            }
+            else turn += 1;
+        }
     }
-    
     return answer;
 }
