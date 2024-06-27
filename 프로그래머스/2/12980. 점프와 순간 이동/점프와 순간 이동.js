526982630185eb0c4e16ea780fe1736821579636
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-06-27 15:18:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = 0;
+    while(n > 0){
+        if(Number.isInteger(n / 2)){
+            n = n / 2;
+        }
+        else {
+            n -= 1;
+            answer += 1;
+        }
+    }
+    return answer;
+}
