21ec70383a71a5ee070d5177d0aa71485e71ca72
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-06-30 21:07:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,17 +1,8 @@
-function isUpperCase(str) {
-  return str === str.toUpperCase();
-}
 function solution(s) {
     var answer = '';
     for(let i = 0; i < s.length; i++){
-        if(i === 0 || s[i-1] === " "){
-            if(isNaN(s[i])) answer += s[i].toUpperCase();
-            else answer += s[i];
-        }
-        else{
-            if(isUpperCase(s[i])) answer += s[i].toLowerCase();
-            else answer += s[i];
-        }
+        if(i === 0 || s[i - 1] === ' ') answer += s[i].toUpperCase();
+        else answer += s[i].toLowerCase();
     }
     return answer;
 }
