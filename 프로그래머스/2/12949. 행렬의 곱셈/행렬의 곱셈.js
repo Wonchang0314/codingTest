ea12b2b7ded9bb5be6928e1c7f8bef34bd08bfc8
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2024-07-01 15:59:53 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.74 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,17 +1,18 @@
+
 function solution(arr1, arr2) {
-    let rowOne = arr1.length, colOne = arr1[0].length;
-    let rowTwo = arr2.length, colTwo = arr2[0].length;
-    let answer = new Array(rowOne);
-    
-    for(let i = 0; i < rowOne; i++){
-        answer[i] = new Array(colTwo).fill(0);
-        for(let j = 0; j < colTwo; j++){
-            let sum = 0;
-            for(let k = 0; k < colOne; k++){
-                sum += arr1[i][k] * arr2[k][j];   
-            }
-            answer[i][j] = sum;
-        }   
+    var answer = [];
+    for (let i = 0; i < arr1.length; i++) {
+        answer[i] = new Array(arr2[0].length).fill(0);
     }
-    return answer
+    
+    for(let i = 0; i < arr1.length; i++){
+        for(let j = 0; j < arr2[0].length; j++){
+            let current = 0;
+            for(let k = 0; k < arr1[0].length; k++){
+                current += arr1[i][k] * arr2[k][j];
+            }
+            answer[i][j] = (current);
+        }
+    }
+    return answer;
 }
