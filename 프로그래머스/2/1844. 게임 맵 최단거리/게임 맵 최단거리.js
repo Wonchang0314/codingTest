ede4712aca9910683765d97b1b79b83ff74a7bd3
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2025-02-16 16:32:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 39.34 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,38 @@
-const isValid = (ny, nx, n, m, maps) => {
-    return ny >= 0 && ny < n && nx >= 0 && nx < m && maps[ny][nx] !== 0;
+const check = (x, y, n, m, maps) => {
+    return x >= 0 && x < n && y >= 0 && y < m && maps[x][y] === 1;
 }
+
 function solution(maps) {
-    let answer = [];
+    let answer = 0;
     const n = maps.length;
     const m = maps[0].length;
-    const dy = [0, 0, -1, 1];
-    const dx = [-1, 1, 0, 0];
+    const targetX = n - 1;
+    const targetY = m - 1;
     
-    const visited = Array.from({ length: n }, () => Array(m).fill(false));
-    const queue = [[0, 0, 1]];
+    const queue = [];
+    const visited = Array.from({length: n}, () => Array(m).fill(false));
+    const directions = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+    queue.push([0, 0, 1]);
     visited[0][0] = true;
     
-    while(queue.length > 0){
-        const [y, x, distance] = queue.shift();
+    while(queue.length) {
+        const [x, y, dist] = queue.shift();
         
-        if(y === n - 1 && x === m - 1) return distance; 
+        if(x === targetX && y === targetY) {
+            return dist;
+        }
         
-        for(let i = 0; i < 4; i++){
-            const ny = y + dy[i];
-            const nx = x + dx[i];
-            
-            if(isValid(ny, nx, n, m, maps) && !visited[ny][nx]){
-                visited[ny][nx] = true;
-                queue.push([ny, nx, distance + 1]);
+        for(const [dx, dy] of directions) {
+            const nextX = x + dx;
+            const nextY = y + dy;
+            if(check(nextX, nextY, n, m, maps) && !visited[nextX][nextY]) {
+                visited[nextX][nextY] = true;
+                queue.push([nextX, nextY, dist + 1]);
             }
         }
     }
+    
+    
     
     return -1;
 }
