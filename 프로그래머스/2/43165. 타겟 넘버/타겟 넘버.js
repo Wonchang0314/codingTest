788f484c9c60d032dc5238c10cb130e4a26aa575
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2025-02-16 16:15:57 +0900

    [level 2] Title: 타겟 넘버, Time: 41.33 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,14 @@
 function solution(numbers, target) {
     var answer = 0;
-    let index = 0;
-    function recursion (num, index){
-        if(index === numbers.length){
-            if(num === target){
-                answer += 1;
-            }
+
+    const dfs = (index, sum) => {
+        if(index === numbers.length) {
+            if(sum === target) answer += 1;
             return;
         }
-        recursion(num + numbers[index], index + 1);
-        recursion(num - numbers[index], index + 1);
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
-    recursion(0, 0);
+    dfs(0, 0);
     return answer;
 }
