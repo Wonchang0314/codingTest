440e9c51f8085c2a0e1321eb3c08ff57366f1abd
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2025-02-19 16:20:32 +0900

    [level 1] Title: 실패율, Time: 9.33 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,17 +1,23 @@
 function solution(N, stages) {
     var answer = [];
+  
     let failRate = {};
-    
-    for(let i = 1; i <= N; i++){
+    for(let i = 1; i <= N; i++) {
         failRate[i] = 0;
-        const filterd = stages.filter(e => e > i);
-        failRate[i] = (stages.length - filterd.length) / stages.length; 
-        stages = filterd;
     }
-    failRate = Object.entries(failRate).sort(([, a], [, b]) => b - a);
-    failRate.forEach(e => {
-        answer.push(Number(e[0]));
-    })
+    for(const stage of stages) { 
+        if(stage > N) continue;
+        failRate[stage] += 1;
+    }
     
+    let peoples = stages.length;
+    for(const [key, value] of Object.entries(failRate)) {
+        const temp = value / peoples;
+        peoples -= value;
+        failRate[key] = temp;
+    }
+    
+    answer = Object.entries(failRate).sort((a, b) => b[1] - a[1]);
+    answer = answer.map(e => Number(e[0]));
     return answer;
 }
