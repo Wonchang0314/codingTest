7f51fb556d01864a87336701d9ed385d8560fcd1
Wonchang0314 <andrew0314@naver.com>
2025-05-16 12:57:32 +0900

    leetcode 417 upate

--- 리트코드/BFS/417.js
+++ 리트코드/BFS/417.js
@@ -1,3 +1,4 @@
+// bfs로 풀기
 /**
  * @param {number[][]} heights
  * @return {number[][]}
@@ -60,6 +61,64 @@ var pacificAtlantic = function (heights) {
     for (let c = 0; c < m; c++) {
       const current = `${r},${c}`;
       if (pacific.has(current) && atlantic.has(current)) {
+        answer.push([r, c]);
+      }
+    }
+  }
+
+  return answer;
+};
+
+// dfs로 풀기
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function (heights) {
+  const n = heights.length;
+  const m = heights[0].length;
+  const p = new Set();
+  const a = new Set();
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  const dfs = (r, c, visited, prevHeight) => {
+    const current = `${r},${c}`;
+    if (
+      r < 0 ||
+      r >= n ||
+      c < 0 ||
+      c >= m ||
+      visited.has(current) ||
+      heights[r][c] < prevHeight
+    ) {
+      return;
+    }
+    visited.add(current);
+    for (let [dr, dc] of directions) {
+      dfs(r + dr, c + dc, visited, heights[r][c]);
+    }
+  };
+
+  for (let i = 0; i < n; i++) {
+    dfs(i, 0, p, heights[i][0]);
+    dfs(i, m - 1, a, heights[i][m - 1]);
+  }
+
+  for (let i = 0; i < m; i++) {
+    dfs(0, i, p, heights[0][i]);
+    dfs(n - 1, i, a, heights[n - 1][i]);
+  }
+
+  const answer = [];
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < m; c++) {
+      const current = `${r},${c}`;
+      if (p.has(current) && a.has(current)) {
         answer.push([r, c]);
       }
     }
