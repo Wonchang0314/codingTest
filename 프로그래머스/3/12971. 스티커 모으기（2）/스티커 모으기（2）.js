c6658d20117aa6c7a60c844993bff895a26a3dba
Wonseok Chang <122918159+Wonchang0314@users.noreply.github.com>
2025-05-20 16:19:59 +0900

    [level 3] Title: 스티커 모으기(2), Time: 6.86 ms, Memory: 39.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -0,0 +1,25 @@
+function solution(sticker) {
+    var answer = 0;
+    const n = sticker.length;
+    if(n === 0) return 0;
+    if(n === 1) return sticker[n - 1];
+    
+    const dp1 = new Array(n).fill(0);
+    dp1[0] = sticker[0];
+    dp1[1] = sticker[0];
+    
+    for(let i = 2; i < n - 1; i++) {
+        dp1[i] = Math.max(dp1[i - 1], sticker[i] + dp1[i - 2]);
+    }
+    
+    const dp2 = new Array(n).fill(0);
+    dp2[0] = 0;
+    dp2[1] = sticker[1];
+    
+    for(let i = 2; i < n; i++) {
+        dp2[i] = Math.max(dp2[i - 1], sticker[i] + dp2[i - 2]);
+    }
+    
+    answer = Math.max(dp1[n - 2], dp2[n - 1]);
+    return answer;
+}
